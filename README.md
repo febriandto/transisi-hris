604f316742dadcd643a8ba27c4cb9a372ba141c2
febriandto <febri031@gmail.com>
2022-01-16 07:51:30 +0700

    delete all

--- README.md
+++
@@ -1,2 +0,0 @@
-# syncrum
-Project Warehouse System Syncrum
--- sys/resources/views/partials/breadcrumb.blade.php
+++
@@ -1,4 +0,0 @@
-@section('breadcrumb')
-	<a class="btn" href="{{ $route_all_data }}"><i class="icon-list"></i>&nbsp; All Data</a>
-	<a class="btn" href="{{ $route_trash }}"><i class="icon-trash"></i>&nbsp; Trash</a>
-@stop
